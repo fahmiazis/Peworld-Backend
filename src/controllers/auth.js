22ca0aaf691ddf4fc1de72e62f3dec256fac11e3
prmstrgt <pramestiregitaa@gmail.com>
2020-11-30 17:28:31 +0700

    register for Company

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,20 +1,20 @@
-const { Users, UserDetails } = require('../models')
+const { Users, UserDetails, Company } = require('../models')
 
 const bcrypt = require('bcrypt')
 
 const response = require('../helpers/response')
-const { register } = require('../helpers/validation')
+const { registerSeeker, registerCompany } = require('../helpers/validation')
 
 module.exports = {
   register: async (req, res) => {
     try {
       const { role } = req.params
-      const { value, error } = register.validate(req.body)
-      if (error) {
-        return response(res, error.message, {}, 400, false)
-      }
       switch (role) {
         case 'job-seeker': {
+          const { value, error } = registerSeeker.validate(req.body)
+          if (error) {
+            return response(res, error.message, {}, 400, false)
+          }
           let { name, email, phone, password } = value
           password = await bcrypt.hash(password, await bcrypt.genSalt())
 
@@ -27,6 +27,30 @@ module.exports = {
             const createDetails = await UserDetails.create(details)
             if (createDetails) {
               return response(res, 'User created!', { data: { id: createUser.id, value } }, 201)
+            } else {
+              return response(res, 'Failed to create user', {}, 400, false)
+            }
+          }
+          break
+        }
+        case 'company': {
+          const { value, error } = registerCompany.validate(req.body)
+          if (error) {
+            return response(res, error.message, {}, 400, false)
+          }
+          console.log(value)
+          let { name, email, company, jobDesk, phone, password } = value
+          password = await bcrypt.hash(password, await bcrypt.genSalt())
+
+          const users = { email, password, roleId: 2 }
+
+          const createUser = await Users.create(users)
+          if (createUser) {
+            const details = { name, company, jobDesk, phone, userId: createUser.id }
+
+            const createDetails = await Company.create(details)
+            if (createDetails) {
+              return response(res, 'User created!', { data: { id: createUser.id, ...value } }, 201)
             } else {
               return response(res, 'Failed to create user', {}, 400, false)
             }
--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -4,9 +4,18 @@ module.exports = {
   role: joi.object({
     name: joi.string().required()
   }),
-  register: joi.object({
+  registerSeeker: joi.object({
     name: joi.string().required(),
     email: joi.string().required(),
+    phone: joi.string().required(),
+    password: joi.string().required().min(8),
+    confirmPassword: joi.string().required().min(8).equal(joi.ref('password'))
+  }),
+  registerCompany: joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    company: joi.string().required(),
+    jobDesk: joi.string().required(),
     phone: joi.string().required(),
     password: joi.string().required().min(8),
     confirmPassword: joi.string().required().min(8).equal(joi.ref('password'))
