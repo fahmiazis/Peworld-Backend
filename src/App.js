2787ac6b02b019d1cd2d1a4e068ee05a251feea6
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-30 14:46:22 +0800

    Project set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+const { APP_PORT } = process.env
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(cors())
+
+app.listen(APP_PORT, () => {
+  console.log(`App is running on port ${APP_PORT}`)
+})
