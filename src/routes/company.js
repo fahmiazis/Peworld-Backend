192bcb7444408aa174890bc6c4958a1635372a41
prmstrgt <pramestiregitaa@gmail.com>
2020-12-01 14:51:41 +0700

    create route for company to message

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,3 +1,9 @@
 const route = require('express').Router()
 
+const messageController = require('../controllers/message')
+
+route.post('/message/:id', messageController.sendMsg)
+route.get('/message/list', messageController.listMsg)
+route.get('/message/:id', messageController.listChat)
+
 module.exports = route
