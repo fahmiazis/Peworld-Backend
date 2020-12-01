8f8f422a6af82d7f12a2a3e134a181e971cd692a
fahmiismail <kewekkewek57@gmail.com>
2020-12-01 10:46:18 +0700

    create function get all job sekker and get detail job-seeker

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,3 +1,7 @@
 const route = require('express').Router()
+const jobSeek = require('../controllers/jobSeekers')
+
+route.get('/job-seeker/all', jobSeek.getUsers)
+route.get('/job-seeker/:id', jobSeek.getDetailUser)
 
 module.exports = route
