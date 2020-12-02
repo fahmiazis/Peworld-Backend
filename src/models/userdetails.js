87b1ca96de787c69afab56633ac044bffcbe62c8
timot24 <timotiussimanjuntak24@gmail.com>
2020-12-02 15:12:10 +0700

    fix typo in aliases model imageprofile

--- src/models/userdetails.js
+++ src/models/userdetails.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       // define association here
       UserDetails.hasOne(models.ImageProfile, {
-        as: 'avatar',
+        as: 'profileAvatar',
         foreignKey: 'userId'
       })
 
