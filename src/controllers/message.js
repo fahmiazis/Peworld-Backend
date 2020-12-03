04a165eaf3c08a9763e2e40683cdffdd9b521ef8
prmstrgt <pramestiregitaa@gmail.com>
2020-12-03 18:54:20 +0700

    edit pagination message

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -70,7 +70,7 @@ module.exports = {
         }
       })
 
-      const pageInfo = pagination('job-seeker/message/list', req.query, page, limit, count)
+      const pageInfo = pagination(req.originalUrl.split('?')[0], req.query, page, limit, count)
 
       const results = await Message.findAll({
         where: {
@@ -164,7 +164,7 @@ module.exports = {
         }
       })
 
-      const pageInfo = pagination(`job-seeker/message/${friendId}`, req.query, page, limit, count)
+      const pageInfo = pagination(req.originalUrl.split('?')[0], req.query, page, limit, count)
 
       const read = await Message.update({ isRead: true }, {
         where: {
